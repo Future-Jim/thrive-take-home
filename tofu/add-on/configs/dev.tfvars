bcebfb13a95e26dce46ea506a55c0a003817ef41
james arias <ariasjames@gmail.com>
2025-08-25 15:03:46 -0400

    bump

--- tofu/add-on/configs/dev.tfvars
+++ tofu/add-on/configs/dev.tfvars
@@ -1,3 +1,3 @@
-s3_bucket = "tf-backend-671"
+s3_bucket = "tf-backend-11791"
 s3_key = "thrive/infra/terrafrom.tfstate"
-aws_account_id = "058264163591"
+aws_account_id = "767398143316"
--- tofu/add-on/providers.tf
+++ tofu/add-on/providers.tf
@@ -8,7 +8,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket       = "tf-backend-671"
+    bucket       = "tf-backend-11791"
     key          = "thrive/add-on/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
