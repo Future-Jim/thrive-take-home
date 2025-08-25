3cff6b969df1d340c0885dffc06f98f096a7c6de
james arias <ariasjames@gmail.com>
2025-08-25 17:13:40 -0400

    bump

--- src_apps/main.go
+++ src_apps/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello Elliott! This is running in EKS via ECR!\n")
+	fmt.Fprintf(w, "Hello Thrive People! This is running in EKS via an artifact in ECR!\n")
 }
 
 func main() {
--- tofu/infra/irsa.tf
+++ tofu/infra/irsa.tf
@@ -11,18 +11,3 @@ module "alb_irsa" {
   }
   tags = local.tags
 }
-
-
-# module "fluent_irsa" {
-#   source           = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-#   version          = "~> 5.30"
-#   role_name        = "${var.project_name}-fluent-bit"
-#   role_policy_arns = ["arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"]
-#   oidc_providers = {
-#     main = {
-#       provider_arn               = module.eks.oidc_provider_arn
-#       namespace_service_accounts = ["logging:aws-for-fluent-bit"]
-#     }
-#   }
-#   tags = local.tags
-# }
